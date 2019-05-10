96eacd2bd4c3f76ce8ad2b22d1e1037061213e3b
bchang <bchang@fulcrm.com>
2019-05-10 19:57:14 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import VueCookies from 'vue-cookies'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+Vue.use(VueCookies)
 
 Vue.config.productionTip = false
 
