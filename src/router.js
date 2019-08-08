76927d38c753f38be7f8c9e480c4fbda78055f4d
bchang <bchang@afterinc.com>
2019-08-08 16:26:12 +0800

    update

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './views/Home.vue'
-import Calculation from './views/Calculation.vue'
-import Main2 from './views/Main2.vue'
-import Main2Level1 from './views/main2/level1.vue'
-import Main2Level2ing from './views/main2/level2ing.vue'
-import Main2Level2 from './views/main2/level2.vue'
+import Calculation from './views/Calculation'
+import Main2 from './views/Main2'
+import Main2Level1 from './views/main2/level1'
+import Main2Level2ing from './views/main2/level2ing'
+import Main2Level2 from './views/main2/level2'
+import Main2Level1_5 from './views/main2/level1-5'
+import Anniversary from './views/main2/anniversary'
 
 Vue.use(VueRouter);
 
@@ -31,6 +33,14 @@ export default new VueRouter({
       {
         path: 'level2',
         component: Main2Level2
+      },
+      {
+        path: 'level1-5',
+        component: Main2Level1_5
+      },
+      {
+        path: 'anniversary',
+        component: Anniversary
       }
     ]
   }]
--- src/views/Main2.vue
+++ src/views/Main2.vue
@@ -5,6 +5,8 @@
         <router-link to="/main2/level1" class="navbar-item bd-navbar-item-videos" role="menuitem">序章</router-link>
         <router-link to="/main2/level2ing" class="navbar-item bd-navbar-item-videos" role="menuitem">第二章戰鬥中</router-link>
         <router-link to="/main2/level2" class="navbar-item bd-navbar-item-videos" role="menuitem">第二章戰前戰後</router-link>
+        <router-link to="/main2/level1-5" class="navbar-item bd-navbar-item-videos" role="menuitem">第二章1~5</router-link>
+        <router-link to="/main2/anniversary" class="navbar-item bd-navbar-item-videos" role="menuitem">週年慶劇情</router-link>
       </div>
     </div>
     <router-view/>
